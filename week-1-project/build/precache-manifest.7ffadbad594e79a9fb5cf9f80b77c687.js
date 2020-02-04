eb520cb81a2f3dc09943444b3a50956d50d03c8a
Masha <4uikina@gmail.com>
2020-02-04 11:05:54 +0100

    build and deploy

---
+++ week-1-project/build/precache-manifest.7ffadbad594e79a9fb5cf9f80b77c687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761e63cbeda1e7979109580ee5fffa3b",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/index.html"
+  },
+  {
+    "revision": "873f856c73b73935d50c",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/2.c6bafbf7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/2.c6bafbf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5cc3cd45f46ed9e99f06",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/main.f584bffe.chunk.js"
+  },
+  {
+    "revision": "338f801eb80f6348d8cd",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/runtime-main.bc4eb59e.js"
+  }
+]);
--- week-1-project/src/props.js
+++ week-1-project/src/props.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App(props) {
     return (
         <section id="app">
-            <h1 data-testid="title">{props.title}</h1>
+            <h1 data-testid="title" id='title'>{props.title}</h1>
             {props.addDescription === true ? <p data-testid="description">This is a description of my app</p> : null}
             {props.articles ? props.articles.map((article, i) => <NewsArticle key = {i} ArtTitle = {article.title} text = {article.text} />) : null}
             {props.articles ? (
@@ -19,6 +19,7 @@ function NewsArticle(props) {
     return (
         <article data-testid="news-article">
             <h1>{props.ArtTitle}</h1>
+            <p>{props.text}</p>
         </article>
     );
 }
